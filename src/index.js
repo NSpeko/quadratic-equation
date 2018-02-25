75224ed7e54d9982fc71fbdc15816ee633defc06
NSpeko <nonstoper42@gmail.com>
2018-02-25 21:00:50 +0300

    Fix: Removed redundant code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
     equation=equation.replace(/ /g,'');
     let tempStr=equation.slice(equation.indexOf('*')+4,equation.length);
     let a=Number.parseInt(equation.slice(0,equation.indexOf('*')));
@@ -8,11 +7,6 @@ module.exports = function solveEquation(equation) {
     let discr = (b * b) - 4 * (a * c);
     let sqrDiscr = Math.sqrt(discr);
     console.log(Math.round((- b + sqrDiscr) / (2 * a)),Math.round((- b - sqrDiscr) / (2 * a)));
-    return Array.from([Math.round((- b + sqrDiscr) / (2 * a)),Math.round((- b - sqrDiscr) / (2 * a))]).sort((num1,num2)=>{return num1-num2;});
-
-    console.log(Number.parseInt(equation.slice(0,equation.indexOf('*'))));
-    console.log(Number.parseInt(tempStr));
-    console.log(tempStr.slice(tempStr.indexOf('x'),tempStr.length));
-    console.log(Number.parseInt(tempStr.slice(tempStr.indexOf('x')+1,tempStr.length)));
-    console.log(a,b,c);
+    return Array.from([Math.round((- b + sqrDiscr) / (2 * a)),Math.round((- b - sqrDiscr) / (2 * a))])
+        .sort((num1,num2)=>{return num1-num2;});
 }
